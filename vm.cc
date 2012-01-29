a72c915410d9a3d6c26b0d0979bf4cf4fbcad06e
Tokuhiro Matsuno <tokuhirom@gmail.com>
2012-01-29 11:15:31 +0900

    init

--- vm.cc
+++ vm.cc
@@ -215,6 +215,7 @@ void VM::execute() {
                 stack.push(v);
             } else { // TODO: remove this and use 'my' keyword
                 v = new Value();
+                v->value_type = VALUE_TYPE_NIL;
                 lexical_vars_stack->back()->setVar(new std::string(op->operand.str_value), v);
                 v->retain();
                 stack.push(v);
