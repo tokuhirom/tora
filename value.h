7ea9a14bc0d40cfa7c5fce861d8bc02e75a2c9d2
Tokuhiro Matsuno <tokuhirom@gmail.com>
2012-01-29 11:14:41 +0900

    better error message

--- value.h
+++ value.h
@@ -125,6 +125,8 @@ public:
             return v;
         }
         default:
+            printf("to_i is not supported yet in not VALUE_TYPE_INT(%d)\n", value_type);
+            this->dump();
             abort();
         }
     }
