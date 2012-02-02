0bb166f3ad1ab2bb6bc2753fe72ba0214dc36090
Tokuhiro Matsuno <tokuhirom@gmail.com>
2012-02-03 08:22:22 +0900

    array!

--- array.cc
+++ array.cc
@@ -16,19 +16,15 @@ Value *ArrayValue::get_item(Value *index) {
 }
 
 void ArrayValue::set_item(Value *index, Value *v) {
-    DBG("SET!!! %d\n", this->values->size());
     IntValuePtr iv(index->to_i());
     int i = iv->int_value;
-    DBG("XXX %d, %d\n", this->values->size(), i);
     if (this->values->size()-1 < i) {
-        DBG("XXY %d, %d\n", this->values->size(), i);
         for (int j=this->values->size()-1; j<i-1; j++) {
-        DBG("put undef %d\n", j);
             this->values->push_back(UndefValue::instance());
         }
+    } else {
+        this->values->erase(this->values->begin()+i);
     }
     this->values->insert(this->values->begin()+i, v);
-    // (*(this->values))[i] = v;
-    this->dump();
 }
 
