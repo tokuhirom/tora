c88daf2bea82587d80cd6cca9a7f9997499ed8cd
Tokuhiro Matsuno <tokuhirom@gmail.com>
2012-02-15 10:17:48 +0900

    range iterator

--- src/value/range.h
+++ src/value/range.h
@@ -14,6 +14,7 @@ public:
     RangeValue(SharedPtr<IntValue> l, SharedPtr<IntValue> r) {
         left = l;
         right = r;
+        value_type = VALUE_TYPE_RANGE;
     }
     void dump(int indent) {
         print_indent(indent);
@@ -21,6 +22,23 @@ public:
     }
     SharedPtr<StrValue> to_s();
     const char *type_str() { return "range"; }
+
+    class iterator : public Value {
+    public:
+        int counter;
+        SharedPtr<RangeValue> parent;
+        iterator(SharedPtr<RangeValue> parent_) {
+            parent = parent_;
+            counter = parent->left->int_value;
+            value_type = VALUE_TYPE_RANGE_ITERATOR;
+        }
+        void dump(int indent) {
+            print_indent(indent);
+            printf("[dump] range_iterator(%d):\n", counter);
+            parent->dump(indent+1);
+        }
+        const char *type_str() { return "array_iterator"; }
+    };
 };
 
 };
