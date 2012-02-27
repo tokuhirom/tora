996a168bf69763769a399cbfcab16be89e434d34
Tokuhiro Matsuno <tokuhirom@gmail.com>
2012-02-28 07:13:16 +0900

    no warn

--- src/token.gen.pl
+++ src/token.gen.pl
@@ -6,8 +6,9 @@ use 5.010000;
 use autodie;
 
 open my $fh, '<', 'src/parser.h';
-open my $ofh, '>', 'src/token.gen.h';
-print $ofh qq!static const char *token_id2name[] = {"EOF",\n!;
+open my $ofh, '>', 'src/token.gen.cc';
+print $ofh qq!#include "token.gen.h"\n!;
+print $ofh qq!const char *tora::token_id2name[] = {"EOF",\n!;
 while (<$fh>) {
     chomp;
     my @a = split /[ \t]+/, $_;
