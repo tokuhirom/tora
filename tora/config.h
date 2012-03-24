b684cf09dd7280db2d2da2a13334af5200dba491
Tokuhiro Matsuno <tokuhirom@gmail.com>
2012-03-24 15:36:34 +0900

    remove config.h from repo

--- tora/config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#define TORA_CCFLAGS "-Wall -Wno-sign-compare -Ivendor/boost_1_49_0/ -I./vendor/re2/ -fstack-protector -march=native -g -DNDEBUG -O2"
-#define TORA_PREFIX  "/usr/local/"
-#define TORA_VERSION_STR  "0.0.3"
