93e938655d3a5475018ee074dd1389194ece04d4
tokuhirom <tokuhirom@gmail.com>
2012-03-28 05:37:43 +0900

    docs

--- tora/object/dynaloader.cc
+++ tora/object/dynaloader.cc
@@ -6,6 +6,20 @@
 
 using namespace tora;
 
+/**
+ * class DynaLoader
+ * 
+ * This is a dynamic extension library loader for tora.
+ * This is a low level library.
+ * You don't need to use this module directly in normal case. You can use XSLoader instead.
+ */
+
+/**
+ * DynaLoader.load(Str $filename, Str $endpoint) : Undef
+ *
+ * Load a extension library from file $filename.
+ * This method loads $filename by dlopen(3) and find symbol named $endpoint, and call it.
+ */
 static SharedPtr<Value> dynaloader_load(VM *vm, Value *self, Value *filename_v, Value *endpoint_v) {
     SharedPtr<StrValue> filename_s = filename_v->to_s();
     SharedPtr<StrValue> endpoint_s = endpoint_v->to_s();
